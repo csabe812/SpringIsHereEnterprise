1efc8bb0eb482d0f87946bfb7d144db0d8caa695
csabe812 <csafulop@gmail.com>
2019-03-03 19:36:16 +0100

    Added a bean, application.properties files and edited some stuffs

--- src/main/java/com/spring/is/here/SpringIsHereApplication.java
+++ src/main/java/com/spring/is/here/SpringIsHereApplication.java
@@ -2,12 +2,17 @@ package com.spring.is.here;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
+@EnableConfigurationProperties
 @SpringBootApplication
 public class SpringIsHereApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringIsHereApplication.class, args);
+		ApplicationContext ct = SpringApplication.run(SpringIsHereApplication.class, args);
+		System.out.println(ct.getBean("person"));
 	}
 
 }
