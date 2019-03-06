c2b1cfa5ebfbab2d0722c8debbea968a2ac0822c
csabe812 <csafulop@gmail.com>
2019-03-06 19:04:47 +0100

    Edited

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 insert into Shop (id, name) values (1,'Walmarkt');
+insert into Shop (id, name) values (2,'Auchan');
 insert into Product(id, name, price, description, shop_id) values (1,'Absinthe', 1000, '80%', (select id from shop where name = 'Walmarkt'));
 insert into Product(id, name, price, description, shop_id) values (2,'vodka', 200, '40%', (select id from shop where name = 'Walmarkt'));
-insert into Shop (id, name) values (2,'Auchan');
 insert into Product(id, name, price, description, shop_id) values (3,'Absinthe', 555, '80%', (select id from shop where name = 'Auchan'));
 insert into Product(id, name, price, description, shop_id) values (4,'Kalinka', 525, '35%', (select id from shop where name = 'Auchan'));
